eb5c3d5ba4008203d8af573796b030280a28c80a
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-09-21 10:35:15 -0300

    middleware json

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ const app=express();
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(cors());
 app.use('/', indexRouter);
 app.use('/posts',postRouter);
